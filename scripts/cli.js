86098e4ee73de9c616fc8bde92bf60ba508ffe28
Khaazz <khaaz.dev@gmail.com>
2020-01-12 21:12:29 +0100

    Refactor CLI. Work with the new system. Ready to add more options

--- scripts/cli.js
+++ scripts/cli.js
@@ -5,55 +5,113 @@ const shell = require('shelljs');
 
 shell.config.fatal = true;
 
-const types = {
-    'esm': 0,
-    'commonjs': 1,
-}
+const libs = ['eris', 'discordjs'];
+const types = ['commonjs', 'esm'];
 
-const [,, ...args] = process.argv;
+const options = {
+    location: process.cwd(),
+    lib: null,
+    type: 'commonjs',
+    token: '',
+    db: 0,
+};
 
-console.log('=== Starting project initialisation ===');
+const flags = {
+    '-l': 'lib',
+    '--lib': 'lib',
+    '-t': 'type',
+    '--type': 'type',
+};
 
-let typeName = 'esm';
-let appName = null;
+function parseArgs(args) {
+    for (let i = 0; i < args.length; i++) {
+        const flag = flags[args[i]];
 
-for (let i = 0; i < args.length; i++) {
-    if (args[i] === '-t' || args[i] === '--type') {
-        typeName = args[i + 1];
-        i++;
-    } else {
-        appName = args[i];
+        if (flag) {
+            options[flag] = args[i + 1];
+            i++;
+        } else {
+            options.location = `${options.location}/${args[i]}`;
+        }
     }
 }
 
-const type = types[typeName] || 0;
+const [
+    ,, ...args
+] = process.argv;
 
-const projectLocation = appName
-    ? `${process.cwd()}/${appName}`
-    : process.cwd();
+console.log('=== Starting project initialisation ===');
 
-const rootLocation = path.resolve(
-    __dirname, '..', 
-    type === 0 ? 'config-esm/*' : 'config-commonjs/*'
+parseArgs(args);
+
+// exit if no no lib
+if (!options.lib || !libs.includes(options.lib) ) {
+    console.error(`Please specify a valid library. Pick one among: ${libs.join(', ')}.`);
+    process.exit(0);
+}
+
+if (!types.includes(options.type) ) {
+    console.error(`Please specify a valid module structure type. Pick one among: ${types.join(', ')}. Default to 'commonjs'.`);
+    options.type = 'commonjs';
+}
+
+const root = path.resolve(__dirname, '..');
+const source = path.resolve(
+    root,
+    options.lib,
+    `${options.lib}-${options.type}/*`,
 );
 
-console.log(`Selecting source: ${typeName}`);
-console.log(`Creating project files at: ${projectLocation}`);
+console.log(`> Selecting source: ${options.lib}-${options.type}`);
+console.log(`> Creating project files at: ${options.location}`);
 
 try {
     shell.cp(
         '-Rn',
-        rootLocation,
-        projectLocation
+        source,
+        options.location,
     );
 } catch (err) {
-    shell.mkdir('-p', projectLocation);
+    shell.mkdir('-p', options.location);
     console.log('Creating folder...');
     shell.cp(
-        '-Rn',
-        rootLocation,
-        projectLocation
+        '-R',
+        source,
+        options.location,
     );
 }
+
+console.log('> Creating configs folder...');
+shell.cp(
+    '-R',
+    `${root}/global/configs`,
+    `${options.location}/src/`,
+);
+shell.cp(
+    '-R',
+    `${root}/global/config-${options.lib}.json`,
+    `${options.location}/src/configs/config.json`,
+);
+
+console.log('> Creating start scripts...');
+shell.mkdir('-p', `${options.location}/scripts`);
+shell.cp(
+    '-R',
+    `${root}/global/scripts/start-${options.type}.js`,
+    `${options.location}/scripts/start.js`,
+);
+
+// gitignore
+shell.cp(
+    '-R',
+    `${root}/global/gitignore`,
+    `${options.location}/.gitignore`,
+);
+// eslintrc
+shell.cp(
+    '-R',
+    `${root}/global/eslintrc.json`,
+    `${options.location}/.eslintrc.json`,
+);
 
 console.log('=== Project initialised ===');
